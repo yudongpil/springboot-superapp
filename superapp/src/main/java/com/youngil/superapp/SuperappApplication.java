dc77804ef8075550177ab231fcd8fa5fd5103d6c
dongpil_yu <yoodongphil@naver.com>
2022-10-25 13:46:06 +0900

    Init

---
+++ superapp/src/main/java/com/youngil/superapp/SuperappApplication.java
@@ -0,0 +1,13 @@
+package com.youngil.superapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperappApplication.class, args);
+	}
+
+}
---
+++ superapp/src/test/java/com/youngil/superapp/SuperappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngil.superapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
